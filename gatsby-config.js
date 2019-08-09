739f045aa7cc94cdca0845b20420edb54380cd90
Blake Mealey <blakemealey@gmail.com>
2019-08-08 21:13:31 -0600

    Update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,11 +31,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Chimerical`,
+        short_name: `Chimerical`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1E292C`,
+        theme_color: `#1E292C`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
